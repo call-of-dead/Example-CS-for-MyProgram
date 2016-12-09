331742654e5bb804d5da1af583bbc72ac031f34c
Diman PRO <diman-pro@myprogram.us>
2016-12-09 18:41:45 +0300

    Добавил проект https://myprogram.us/%D0%BE%D0%B1%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8E/%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D1%8B-%D0%BD%D0%B0-c/329-%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%BE%D0%B2%D1%8B%D0%B9-%D1%80%D0%B5%D0%B4%D0%B0%D0%BA%D1%82%D0%BE%D1%80-%D0%BA%D0%BE%D0%BD%D0%B2%D0%B5%D1%80%D1%82%D0%B5%D1%80

---
+++ REGISTRY2WF/REGISTRY2WF-2/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace REGISTRY2WF_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.filesToolStripMenuItem.Text = "Files";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10→2 ",
+            "10→16",
+            "2→10",
+            "2→16",
+            "16→10 ",
+            "16→2"});
+            this.comboBox1.Location = new System.Drawing.Point(0, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(128, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(0, 54);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 204);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ REGISTRY2WPF/REGISTRY2WPF/MainWindow.xaml.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace REGISTRY2WPF
+{
+    struct PointD
+    {
+        public double X;
+        public  double Y;
+        public PointD(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string fileName;
+        private const string PO = "DEdit ";
+        private bool edit = false;
+        private RegistryKey test9999;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (edit)
+            {
+                MessageBoxResult res= MessageBox.Show("Вы не сохранили документ. Сохранить?", "Предупреждение",MessageBoxButton.YesNoCancel);
+                if (res == MessageBoxResult.Cancel)
+                {
+                    return;
+                }
+                if (res == MessageBoxResult.Yes)
+                {
+                    MenuItem_Click_1(this, e);
+                }
+            }
+            System.Windows.Forms.OpenFileDialog openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.fileName = openFileDialog1.FileName;
+                textBox1.IsEnabled = true;
+                textBox1.Text = System.IO.File.ReadAllText(fileName);
+                this.Title = PO + fileName;
+                this.edit = false;
+                this.button1.IsEnabled = true;
+                test9999.SetValue("FileName", fileName);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (fileName != null && fileName != "")
+            {
+                System.IO.File.WriteAllText(fileName, textBox1.Text);
+                this.Title = PO + fileName;
+                edit = false;
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.edit = true;
+            this.Title = PO + fileName + "*";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            test9999 = Registry.CurrentUser.CreateSubKey("DEdit2");
+            comboBox1.SelectedIndex = 0;
+            Object o= test9999.GetValue("FileName");
+            if (o != null)
+            {
+                fileName = (string)o;
+                textBox1.IsEnabled = true;
+                textBox1.Text = System.IO.File.ReadAllText(fileName);
+                this.Title = PO + fileName;
+                this.edit = false;
+                this.button1.IsEnabled = true;
+            }
+            Point x = new Point();
+            o = test9999.GetValue("StartPosX");
+            PointD x1 = new PointD();
+            if (o != null)
+            {
+                x1.X = Double.Parse((string)o);
+            }
+            o = test9999.GetValue("StartPosY");
+            if (o != null)
+            {
+                x1.Y = Double.Parse((string)o);
+            }
+            this.Top = x1.X;
+            this.Left = x1.Y;
+             o = test9999.GetValue("Width");
+             x1 = new PointD(400, 400);
+            if (o != null)
+            {
+                x1.X = Double.Parse((string)o);
+            }
+            o = test9999.GetValue("Height");
+            if (o != null)
+            {
+                x1.Y = Double.Parse((string)o);
+            }
+            this.Width = x1.X;
+            this.Height = x1.Y;
+            textBox1.Focus();
+
+            o = test9999.GetValue("PosRead");
+            if (o != null)
+            {
+                int i = (int)o;
+                int v = 0;
+                o = test9999.GetValue("LengthRead");
+                if (o != null)
+                {
+                    v = (int)o;
+                }
+                this.textBox1.SelectionStart = i;
+                textBox1.SelectionLength = v;
+            }
+        }
+
+
+        string NumToSTR(int t)
+        {
+            string res;
+            if (t >= 0 && t < 10)
+            {
+                res = (char)('0' + t) + "";
+                return res;
+            }
+            res = (char)('A' + (t - 10)) + "";
+            return res;
+        }
+        int CharToInt(char c)
+        {
+            int n = (int)c - '0';
+            if (n < 10)
+            {
+                return n;
+            }
+            switch (c)
+            {
+                case 'A':
+                    return 10;
+                case 'B':
+                    return 11;
+                case 'C':
+                    return 12;
+                case 'D':
+                    return 13;
+                case 'E':
+                    return 14;
+                case 'F':
+                    return 15;
+                default:
+                    throw new Exception();
+            }
+        }
+        string convert(string txt, string original, string news)
+        {
+            string result = "";
+            if (original.Length <= 0)
+            {
+                return txt;
+            }
+            if (news.Length <= 0)
+            {
+                return txt;
+            }
+            if (txt.Length <= 0)
+            {
+                return txt;
+            }
+            int Num = Int32.Parse(original);
+            int New = Int32.Parse(news);
+            int tmp = 1;
+            int res = 0;
+            for (int i = txt.Length - 1; i >= 0; i--)
+            {
+                int n;
+                try
+                {
+                    n = CharToInt(txt[i]);
+                }
+                catch (Exception e)
+                {
+                    return txt;
+                }
+                if (n < 0 || n > Num)
+                {
+                    return txt;
+                }
+                n = n * tmp;
+                tmp = tmp * Num;
+                res += n;
+            }
+            while (res > 0)
+            {
+                int g = res % New;
+                result = NumToSTR(g) + result;
+                res = res / New;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.SelectedText != "")
+            {
+                string re = comboBox1.Text;
+                string[] reg = re.Split('→');
+                textBox1.SelectedText = convert(textBox1.SelectedText, reg[0], reg[1]);
+                this.edit = true;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (edit)
+            {
+                MessageBoxResult res = MessageBox.Show("Вы не сохранили документ. Сохранить?", "Предупреждение", MessageBoxButton.YesNoCancel);
+                if (res == MessageBoxResult.Cancel)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                if (res == MessageBoxResult.Yes)
+                {
+                    MenuItem_Click_1(this, null);
+                }
+
+            }
+            test9999.SetValue("PosRead", textBox1.SelectionStart);
+            test9999.SetValue("LengthRead", textBox1.SelectionLength);
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            if (test9999 == null)
+                return;
+
+            test9999.SetValue("Width", this.ActualWidth);
+            test9999.SetValue("Height", this.ActualHeight);
+        }
+
+        private void Window_LocationChanged(object sender, EventArgs e)
+        {
+            test9999.SetValue("StartPosX", Top );
+            test9999.SetValue("StartPosY", Left );
+        }
+
+        private void textBox1_FocusableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+        }
+    }
+}
